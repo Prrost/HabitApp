30b626f219304d1d763da653188d463f06d68666
mansurserikov <Mansurserikov889@gmail.com>
2025-02-25 06:56:03 +0500

    register

--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Habit = require("../models/Habit");
+const Habit = require("../models/habit");
 const authenticate = require("../middleware/authMiddleware");
 
 const router = express.Router();
